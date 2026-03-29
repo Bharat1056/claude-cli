ba04f8e34460665a461abb07cdf9c2e266042468
Bharat1056 <bharatpanigrahi225@gmail.com>
2026-03-29 22:40:42 +0530

    fix: build command

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,8 +2,7 @@ import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global;
 
-const prisma = new PrismaClient();
-
+const prisma = globalForPrisma.prisma ?? new PrismaClient();
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
