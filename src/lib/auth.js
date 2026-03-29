105a7a24acc5cc4d99914255718c7ddebbd1f73b
Bharat1056 <bharatpanigrahi225@gmail.com>
2026-03-29 09:50:00 +0530

    feat: setup better into express

---
+++ src/lib/auth.js
@@ -0,0 +1,15 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { Prisma } from "../generated/client";
+
+export const auth = betterAuth({
+  database: prismaAdapter(Prisma, {
+    provider: "postgresql",
+  }),
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }
+  }
+});
